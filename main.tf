7357559271af4aa64898fb8ea513b78b64345e58
Joshua Ma <me@joshma.com>
2020-12-17 18:41:00 -0800

    Add support for IAM policies

--- main.tf
+++ main.tf
@@ -6,11 +6,46 @@ resource "aws_launch_template" "lt" {
   instance_type          = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
 
+  iam_instance_profile {
+    name = aws_iam_instance_profile.profile.name
+  }
+
   user_data = base64encode(templatefile("${path.module}/userdata.tpl", {
     api_host  = var.api_host
     api_token = var.api_token
     team_id   = var.team_id
   }))
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name_prefix = "airplane-agent-"
+  role        = aws_iam_role.role.name
+}
+
+resource "aws_iam_role" "role" {
+  name_prefix = "airplane-agent-"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  count      = length(var.managed_policy_arns)
+  role       = aws_iam_role.role.name
+  policy_arn = var.managed_policy_arns[count.index]
 }
 
 resource "aws_autoscaling_group" "asg" {
--- variables.tf
+++ variables.tf
@@ -1,10 +1,4 @@
-variable "agent_ami" {
-  type = map
-  default = {
-    "us-west-2" = "ami-06040278af81a8aac"
-    // TODO: add us-east-1, eu-west-1, us-east-2, us-west-1
-  }
-}
+// Airplane variables
 
 variable "api_host" {
   type    = string
@@ -15,13 +9,23 @@ variable "api_token" {
   type = string
 }
 
+variable "team_id" {
+  type = string
+}
+
+// AWS variables
+
+variable "agent_ami" {
+  type = map
+  default = {
+    "us-west-2" = "ami-06040278af81a8aac"
+    // TODO: add us-east-1, eu-west-1, us-east-2, us-west-1
+  }
+}
+
 variable "aws_region" {
   type    = string
   default = "us-west-2"
-}
-
-variable "team_id" {
-  type = string
 }
 
 variable "instance_type" {
@@ -29,12 +33,18 @@ variable "instance_type" {
   default = "t3.medium"
 }
 
+variable "managed_policy_arns" {
+  type        = list(string)
+  default     = []
+  description = "List of IAM policy ARNs to attach to the instance role"
+}
+
 variable "vpc_security_group_ids" {
-  type        = list
+  type        = list(string)
   description = "List of security group IDs to apply to instances"
 }
 
 variable "vpc_subnet_ids" {
-  type        = list
+  type        = list(string)
   description = "List of subnet IDs to deploy instances into"
 }
